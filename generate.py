04503998bd53f8a502275db0ec37ef403837d289
Žygimantas Bruzgys <zygimantas@bruzgys.eu>
2017-12-20 22:32:08 +0100

    Added script that generates personalized invitations

--- generate.py
+++ generate.py
@@ -1,16 +1,25 @@
 """
-Generates personalized invitations
+Generates personalized invitations.
 """
+import csv, base64
 from jinja2 import Template
 
 def run():
     """
     void main(char* argv[])
     """
-    with open('index.html') as html, open('guests.csv') as csv:
-        template_en = Template(html.read())
-
-
+    for lang in ['en']:
+        with open('index_%s.html' % lang) as html, \
+                open('guests_%s.csv' % lang) as csv_file:
+            template = Template(html.read(), autoescape=True)
+            guest_reader = csv.reader(csv_file)
+            for row in guest_reader:
+                names = row[0]
+                code = base64.b64encode(str.encode(names)).decode()[:5]
+                output_path = 'invitation_%s.html' % code
+                with open(output_path, 'w') as output:
+                    output.write(template.render(name=names))
+                print(names, output_path)
 
 if __name__ == "__main__":
     run()
