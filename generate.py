b1bdbccc1e2506ff16dd5672d36b81f96986ac72
Žygimantas Bruzgys <zygimantas@bruzgys.eu>
2018-01-14 13:14:27 +0100

    2nd :)

--- generate.py
+++ generate.py
@@ -8,6 +8,7 @@ def run():
     """
     void main(char* argv[])
     """
+    used_hashes = []
     for lang in ['en', 'lt', 'ru']:
         with open('index_%s.html' % lang) as html, \
                 open('guests_%s.csv' % lang) as csv_file:
@@ -15,8 +16,11 @@ def run():
             guest_reader = csv.reader(csv_file)
             for row in guest_reader:
                 names = row[0]
-                code = base64.b64encode(str.encode(names)).decode()[-8:-3]
-                output_path = 'invitation_%s.html' % code
+                code = base64.b64encode(str.encode(names)).decode()[-10:-3]
+                while code in used_hashes:
+                    code += "a"
+                used_hashes.append(code)
+                output_path = 'i_%s.html' % code
                 with open(output_path, 'w') as output:
                     output.write(template.render(name=names))
                 print(names + "," + output_path)
